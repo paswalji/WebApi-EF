22e0b51a5b77e36b90f9f7fa8cabe3aa476f92fd
gaurav <paswalji7533@gmail.com>
2025-04-25 00:20:21 +0530

    web api dot net 8

---
+++ webapiwithentityframework/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
+using webapiwithentityframework.Models;
+
+namespace webapiwithentityframework.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+        public UsersController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+       
+        [HttpGet]
+        public IActionResult Get([FromQuery] int limit = 10)
+        {
+            var user = _userContext.Users.Take(limit).ToList();
+            return Ok(new { message = "Ok", data = user});
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string users)
+        {
+            if (users == null)
+            {
+                return BadRequest(new { message = "Enter the User Data."});
+            }
+            _userContext.Users.Add( new Users { name = users});
+            _userContext.SaveChanges();
+            return Ok(new { message = "User Add Successfully."});
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Users users)
+        {
+            if (users == null)
+            {
+                return BadRequest(new { message = "Enter the User Data." });
+            }
+
+            var user = _userContext.Users.Where(x=>x.id == users.id).FirstOrDefault();
+
+            if(user == null)
+            {
+                return BadRequest(new { message = "User not found !" });
+            }
+
+            user.name = users.name;
+            _userContext.SaveChanges();
+            return Ok(new { message = "User update Successfully." });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] int id )
+        {
+            
+            var user = _userContext.Users.Where(x => x.id == id).FirstOrDefault();
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "User not found !" });
+            }
+
+            _userContext.Users.Remove( user );            
+            _userContext.SaveChanges();
+            return Ok(new { message = "User update Successfully." });
+        }
+
+    }
+}
---
+++ webapiwithentityframework/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapiwithentityframework.Models
+{
+    public class Users
+   {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
